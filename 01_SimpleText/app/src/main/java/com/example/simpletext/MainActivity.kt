47fa3bab3b345f1675d89c5f19073ee953b7b928
Daniel Robledo <daniel.canorobledo@gmail.com>
2025-02-25 13:08:35 +0100

    Implemented basic text composable

--- 01_SimpleText/app/src/main/java/com/example/simpletext/MainActivity.kt
+++ 01_SimpleText/app/src/main/java/com/example/simpletext/MainActivity.kt
@@ -4,44 +4,51 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.simpletext.ui.theme.SimpleTextTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SimpleTextTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainActivityContent()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainActivityContent() {
+    GreetingText("world")
+}
+
+@Composable
+fun GreetingText(name: String) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier
+            .clickable(onClick = {})
+            .padding(all = 24.dp)
+            .height(240.dp)
+            .width(200.dp),
+        style = MaterialTheme.typography.headlineSmall,
+        fontWeight = FontWeight.SemiBold
+
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    SimpleTextTheme {
-        Greeting("Android")
-    }
+    MainActivityContent()
 }
